bbf60671f682b95844b5a9f891245dfbb765bddb
Aaron <aaron.weinzierl@posteo.de>
2025-06-04 18:34:42 +0200

    very basic main file and libraries for different systems

--- core/lib/main.dart
+++ core/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'My App',
-      home: const HomePage(), // This is a custom widget in another file
+      home: const HomePageAndroid(), // This is a custom widget in another file
     );
   }
 }

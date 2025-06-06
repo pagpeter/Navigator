e1c7bc286fa4f062005c2dd3809e5063674fb84b
Aaron <aaron.weinzierl@posteo.de>
2025-06-06 15:29:59 +0200

    home page android basic layout, bruda kp was mit den farben abgeht

--- navigator/lib/pages/android/home_page_android.dart
+++ navigator/lib/pages/android/home_page_android.dart
@@ -8,11 +8,47 @@ class HomePageAndroid extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: 
-      [
-        Center(child: Text(page.searchButtonText)),
-      ]
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(flex: 4, child: Center(child: Text('Map'))),
+          DecoratedBox(
+            decoration: BoxDecoration(
+              color: Colors.primaries.last,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20),
+                topRight: Radius.circular(20),
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(24),
+              child: Center(
+                child: DecoratedBox(
+                  decoration: BoxDecoration(
+                    color: Colors.primaries.first,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(24),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Where do you want to go?',
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: NavigationBar(
+        destinations: [
+          NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+          NavigationDestination(icon: Icon(Icons.bookmark), label: 'Saved'),
+        ],
+      ),
     );
   }
 }
